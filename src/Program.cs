609a40ce2a28e695b5c71126e133de028407718b
Thiago Loureiro <thiagoguaru@outlook.com>
2018-05-17 09:30:14 +0200

    Small Changes

--- src/Program.cs
+++ src/Program.cs
@@ -9,8 +9,8 @@ namespace MonitorARM
             Console.Clear();
             Console.WriteLine("Raspberry Bot Started - Checking your builds");
             Console.WriteLine("Connecting to RabbitMQ");
-            var obj = new RabbitMQ.RabbitMQ("AppVeyor");
-            obj.Connect();
+            var objRabbit = new RabbitMQ.RabbitMQ("AppVeyor");
+            objRabbit.Connect();
 
             Console.WriteLine("Connected successfully with RabbitMQ!");
 
